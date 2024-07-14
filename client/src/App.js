00ff5dbe6c76a2393156ef3ff739c6ab4be451ad
V Harshini <harshinivk12@gmail.com>
2024-07-14 21:45:42 +0530

    resolved the node_modules problem

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+// import {Router, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-    
+    <h1>Hello</h1>
     </>
   );
 }
